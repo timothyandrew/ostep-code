e3d36aa31c39f936c96909eeeaa4003e2c2c382f
Timothy Andrew <mail@timothyandrew.net>
2021-05-22 02:30:15 -0700

    multiple file descriptors + fork

--- process-api/main.c
+++ process-api/main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main() {
+  int file = open("/tmp/foo.txt", O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+
+  if (file == -1) {
+    fprintf(stderr, "Failed to open file: %s\n", strerror(errno));
+    exit(1);
+  }
+
   int rc = fork();
-  int x = 42;
 
   if (rc < 0) {
     fprintf(stderr, "Failed to fork!\n");
@@ -14,12 +24,32 @@ int main() {
   } else if (rc == 0) {
     // Child
     sleep(2);
-    x = x + 5;
-    fprintf(stderr, "Hi from child (%d)!\n", x);
+    fprintf(stderr, "Hi from child\n");
+
+    char contents[] = "this is inside the parent!\n";
+    int result = write(file, contents, strlen(contents));
+
+    if (result == -1) {
+      fprintf(stderr, "Failed to write to file: %s\n", strerror(errno));
+      exit(1);
+    }
+
+    close(file);
+
   } else {
     // Parent
+
+    char contents[] = "this is inside the file!\n";
+    int result = write(file, contents, strlen(contents));
+
+    if (result == -1) {
+      fprintf(stderr, "Failed to write to file: %s\n", strerror(errno));
+      exit(1);
+    }
+
     wait(NULL);
-    x = x + 7;
-    fprintf(stderr, "Parent exiting (%d)!\n", x);
+    close(file);
+
+    fprintf(stderr, "Parent exiting\n");
   }
 }
